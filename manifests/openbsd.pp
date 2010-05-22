e2e265c03ea84e826e617223d9b6835b28768844
mh <mh@immerda.ch>
2010-05-22 15:07:47 +0200

    add new latest screen release

--- manifests/openbsd.pp
+++ manifests/openbsd.pp
@@ -1,3 +1,6 @@
 class screen::openbsd {
-    openbsd::special_package{'screen-4.0.3p1': }
+  case $operatingsystemrelease {
+    '4.3': { openbsd::special_package{'screen-4.0.3p1': } }
+    default: { openbsd::special_package{'screen-4.0.3p1': } }
+  }
 }
