705c140aaf25e3e3d7f5436b715b29bff4acb319
mh <mh@immerda.ch>
2012-06-05 18:47:35 -0300

    new style for 2.7

--- manifests/openbsd.pp
+++ manifests/openbsd.pp
@@ -1,5 +1,5 @@
 class screen::openbsd {
-  case $operatingsystemrelease {
+  case $::operatingsystemrelease {
     '4.3': { openbsd::special_package{'screen-4.0.3p1': } }
     default: { openbsd::special_package{'screen-4.0.3p2': } }
   }
