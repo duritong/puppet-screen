dd765ba1ec9de710650db706062bf35dd7ceaf0c
mh <mh@immerda.ch>
2015-11-28 17:41:43 +0100

    linting for future parser

--- manifests/screenrc.pp
+++ manifests/screenrc.pp
@@ -1,3 +1,4 @@
+# manage a screenrc
 define screen::screenrc(
   $source = 'normal',
   $target = '/root/.screenrc',
@@ -6,12 +7,12 @@ define screen::screenrc(
 ){
   require ::screen
   file {$name:
-    path => $target,
-    source =>   [
-      "puppet:///modules/site_screen/${::fqdn}/${source}",
-      "puppet:///modules/site_screen/${source}",
-      "puppet:///modules/screen/${source}"
-    ],
-    owner => $owner, group => $group, mode => 0600;
+    path   => $target,
+    source => [ "puppet:///modules/site_screen/${::fqdn}/${source}",
+                "puppet:///modules/site_screen/${source}",
+                "puppet:///modules/screen/${source}", ],
+    owner  => $owner,
+    group  => $group,
+    mode   => '0600';
   }
 }
